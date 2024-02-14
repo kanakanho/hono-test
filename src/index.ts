14124d934bc17c5e8f4afc958bd9ecdc3f50eb02
kanakanho <kahoshibakichi@gmail.com>
2024-02-14 19:33:36 +0900

    add リクエストヘッダーの中身を返すAPI

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+    // ヘッダーの中身を確認
+    console.log(c);
+    console.log(c.req.raw.headers);
+    const headers = c.req.raw.headers;
+    const auth = headers.get("authorization");
+    console.log(auth);
+    if (!auth) {
+        return c.text("No Authorization header");
+    }
+
+    return c.text(auth);
+});
+
+export default app;
